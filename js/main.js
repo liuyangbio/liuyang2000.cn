a7bee24f85bde2a8ddca819fe4d90725126095e7
shaoyang <liuyangbio@qq.com>
2025-09-26 10:44:27 +0800

    V1.0

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// 平滑滚动
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// 导航栏滚动效果
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
